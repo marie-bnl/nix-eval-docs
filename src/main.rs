a6be9308693be29a20cbf45ea4ae094fda86117e
Marie Bonnelle <marie.bonnelle@proton.me>
2026-09-19 17:09:31 +0200

    Add code using just nix_bindings::sys for experimenting with pointers

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,51 @@
-use std::sync::Arc;
-
-use nix_bindings::{Context, EvalStateBuilder, Store};
 use nix_bindings::sys;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ctx = Arc::new(Context::new()?);
-    let store = Arc::new(Store::open(&ctx, None)?);
-    let state = EvalStateBuilder::new(&store)?.build()?;
+    unsafe {
+        let ctx_ptr = sys::nix_c_context_create();
+        sys::nix_libexpr_init(ctx_ptr);
 
-    let result = state.eval_from_string("
-        {
-            /** Returns the sum of `a` and `b`. */
-            sum = a: b: a + b;
-        }
-    ", "<eval>")?;
+        let store_uri_ptr = std::ptr::null();
+        let store_params_ptr = std::ptr::null_mut();
+        let store_ptr = sys::nix_store_open(ctx_ptr, store_uri_ptr, store_params_ptr);
 
-    /*
-        Something like:
+        let state_builder_ptr = sys::nix_eval_state_builder_new(ctx_ptr, store_ptr);
+        let state_ptr = sys::nix_eval_state_build(ctx_ptr, state_builder_ptr);
 
-            let fun = result.get_attr("sum");
-
-            let doc = unsafe {
-                sys::nix_get_value_doc(
-                    ctx.as_ptr(),
-                    state.as_ptr(),
-                    fun.as_ptr(),
-                );
+        let path_c = std::ffi::CString::new("<eval>")?;
+        let expr_c = std::ffi::CString::new("
+            {
+                /** Returns the sum of `a` and `b`. */
+                sum = a: b: a + b;
             }
+        ")?;
 
-            println!("{}", doc.doc);
+        let value_ptr = sys::nix_alloc_value(ctx_ptr, state_ptr);
+        sys::nix_expr_eval_from_string(
+          ctx_ptr,
+          state_ptr,
+          expr_c.as_ptr(),
+          path_c.as_ptr(),
+          value_ptr,
+        );
 
-        Except it will probably need to be implemented in nix-bindings directly
-        by patching it because `as_ptr`s are private.
+        let key_c = std::ffi::CString::new("sum")?;
+        let sum_attr_ptr = sys::nix_get_attr_byname(
+            ctx_ptr,
+            value_ptr,
+            state_ptr,
+            key_c.as_ptr(),
+        );
 
-        Also sys::nix_get_value_doc doesn't exist for some reason.
-    */
+        // error[E0425]: cannot find function `nix_get_value_doc` in module `sys`
+        let sum_doc = sys::nix_get_value_doc(
+            ctx_ptr,
+            state_ptr,
+            value_ptr,
+        );
+
+        println!("{}", sum_doc.doc);
+    }
 
     Ok(())
 }
